52c235e54322d511a992560cf8d2fb93930435cf
Andreu <corts395@gmail.com>
2020-11-24 13:46:30 +0100

    primer commit

---
+++ models/test.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+
+const Test = mongoose.model('Test', new mongoose.Schema({
+    question: {
+        type: String,
+        required: true,
+        
+        
+        trim: true,
+        
+    },
+    answer1: {
+        type: String,
+        required: true,
+        
+        
+        trim: true,
+        
+    },
+    answer2: {
+        type: String,
+        required: true,
+        
+        
+        trim: true,
+        
+    },
+    answer3: {
+        type: String,
+        required: true,
+        
+        
+        trim: true,
+        
+    },
+    
+}))
+
+module.exports = Test
